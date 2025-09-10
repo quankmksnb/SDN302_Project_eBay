d2415ddc818c9946456c8b404c5213caf17eaa46
Nguyễn Minh Quân <quannguyenkm16122004@gmail.com>
2025-09-10 11:59:58 +0700

    Create project and config routing by file-based and config tailwind

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SDN302_Project_eBay
+
+Clone về check out sang develop và npm i sau đó chạy.
+chạy fe: npm run dev
---
+++ ebay-fe/src/app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className="text-[120px] bg-blue-500">Trang ban đầu</h1>
+  );
+}
