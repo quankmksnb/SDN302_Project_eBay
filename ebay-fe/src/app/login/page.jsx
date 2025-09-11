a612de8fa974dfcd12405837416e50b69a7053ef
VinhHV28 <vinhhv28@gmail.com>
2025-09-11 10:34:23 +0700

    Add common button

--- ebay-fe/src/app/login/page.jsx
+++ ebay-fe/src/app/login/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+import Button from "@/common/Button";
 import {
   LoadingDots,
   LoadingPulse,
@@ -5,22 +7,60 @@ import {
   SimpleLoading,
   SkeletonLoading,
 } from "@/common/Loading";
+import { useState } from "react";
 
 export default function LoginPage() {
-  const isLoading = true;
-  if (isLoading)
-    return (
-      <>
-        <SimpleLoading message={"Loading..."}></SimpleLoading>
-        {/* <LoadingDots></LoadingDots> */}
-        {/* <LoadingPulse></LoadingPulse> */}
-        {/* <SkeletonLoading></SkeletonLoading> */}
-        {/* <OverlayLoading></OverlayLoading> */}
-      </>
-    );
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+  // const isLoading = false;
+  // if (isLoading)
+  //   return (
+  //     <>
+  //       <SimpleLoading message={"Loading..."}></SimpleLoading>
+  //       {/* <LoadingDots></LoadingDots> */}
+  //       {/* <LoadingPulse></LoadingPulse> */}
+  //       {/* <SkeletonLoading></SkeletonLoading> */}
+  //       {/* <OverlayLoading></OverlayLoading> */}
+  //     </>
+  //   );
   return (
     <main>
-      <p className="text-2xl text-white bg-red-500">Bú tailwind màu đỏ</p>
+      <div style={{ padding: 40, background: "" }}>
+        <h1>Test Button Component</h1>
+
+        {/* Primary Button */}
+        {/* <Button
+          size="lg"
+          onClick={handleClick}
+          isLoading={loading}
+          variant="primary"
+        >
+          Shop now
+        </Button> */}
+        <Button
+          onClick={handleClick}
+          isLoading={loading}
+          size="md"
+          variant="light"
+        >
+          Shop now
+        </Button>
+        <div></div>
+        {/* <Button
+          onClick={handleClick}
+          isLoading={loading}
+          size="sm"
+          variant="dark"
+        >
+          Shop now
+        </Button> */}
+      </div>
     </main>
   );
 }
