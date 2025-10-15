235e912e787448889f8dcaf2b57931a236df4ff6
VinhHV28 <vinhhv28@gmail.com>
2025-10-15 09:33:15 +0700

    Update BE: add Category model

---
+++ ebay-be/models/Category.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const categorySchema = mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+export default mongoose.model("Category", categorySchema, "categories");
