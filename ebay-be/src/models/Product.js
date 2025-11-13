6c31ce1fd99f77bf5e2b6824204b49642d7eedfe
DESKTOP-CE0H5MK\Admin <doanhuy2004@gmail.com>
2025-11-13 23:10:37 +0700

    update model product

--- ebay-be/src/models/Product.js
+++ ebay-be/src/models/Product.js
@@ -3,19 +3,31 @@ import User from "./User.js";
 import Category from "./Category.js";
 
 const { Schema } = mongoose;
-const productSchema = mongoose.Schema({
+
+const productSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String },
-  price: { type: Number, required: true },
-  images: [{ type: String }],
+  description: String,
+  price: { type: Number, required: true }, // giá khởi điểm
+  images: [String],
   categoryId: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   sellerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+  // --- Thông tin đấu giá ---
   isAuction: { type: Boolean, default: false },
+  startingPrice: { type: Number, default: 0 }, // giá khởi điểm của phiên đấu giá
+  currentPrice: { type: Number, default: 0 }, // giá hiện tại (cao nhất)
+  highestBidder: { type: Schema.Types.ObjectId, ref: "User" }, // người đang giữ giá cao nhất
+  minIncrement: { type: Number, default: 1 }, // bước giá tối thiểu
   auctionEndTime: {
     type: Date,
     required: function () {
       return this.isAuction;
     },
+  },
+  auctionStatus: {
+    type: String,
+    enum: ["upcoming", "active", "ended"],
+    default: "upcoming",
   },
 });
 
